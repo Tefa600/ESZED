8bbf269931fdcc50ba13be218d82b13cca72e84c
Tefa600 <abdellatifmostafa06@gmail.com>
2023-05-13 21:25:11 +0300

    delete expires

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -187,7 +187,7 @@ export default function Login() {
         const {user} = e.data.data;
         console.log(user);
         window.sessionStorage.setItem('token', e.data.token);
-        Cookies.set("token",e.data.token,{expires:7});
+        Cookies.set("token",e.data.token);
         axios.defaults.headers.common["Authorization"] = `Bearer ${e.data.token}`; // this is how you send token in the Authorization as a header
         const decodedToken = jwtDecode(e.data.token);
         setUserId(decodedToken.userId);

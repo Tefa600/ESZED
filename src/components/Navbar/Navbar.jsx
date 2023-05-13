2adb13a9c9b1ec110137d037a7f5fcf1d3e728ed
Tefa600 <abdellatifmostafa06@gmail.com>
2023-05-13 21:51:56 +0300

    navbar hide show
    depends on the expiration fate of the cookies

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,120 +16,115 @@ export default function Navbar() {
     }, []);
 
     function Logout() {
-        axios.post("api/user/logout").then((e)=>{
+        axios.post("api/user/logout").then((e) => {
             console.log(e.status);
         }).catch()
 
     }
 
-    return (
-        <nav
-            className={`navbar navbar-expand-lg fixed-top navbar-light text-white ${styles.navBg}`}
-        >
+    console.log(Cookies.expires)
 
-            <div className="container-fluid">
-                <div className="logoPic">
-                    <a href="Home" className="logoHome">
-                        <img src={Logo} alt="Space Logo"/>
-                    </a>
+    return (<nav
+        className={`navbar navbar-expand-lg fixed-top navbar-light text-white ${styles.navBg}`}
+    >
 
-                </div>
-                <div>
-                    <div>Hi {userData.userName}  </div>
-                </div>
+        <div className="container-fluid">
+            <div className="logoPic">
+                <a href="Home" className="logoHome">
+                    <img src={Logo} alt="Space Logo"/>
+                </a>
 
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportContent"
-                    aria-controls="navbarText"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className={`navbar-toggler-icon ${styles.togglerIcon}`}/>
-                </button>
-                <div
-                    className={`collapse navbar-collapse  ${styles.items}`}
-                    id="navbarSupportContent"
-                >
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item active ">
-                            <Link className="nav-link text-white" to="Home">
-                                Home <span className="sr-only">(current)</span>
+            </div>
+            <div>
+                <div>Hi {userData.userName}  </div>
+            </div>
+
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportContent"
+                aria-controls="navbarText"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className={`navbar-toggler-icon ${styles.togglerIcon}`}/>
+            </button>
+            <div
+                className={`collapse navbar-collapse  ${styles.items}`}
+                id="navbarSupportContent"
+            >
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item active ">
+                        <Link className="nav-link text-white" to="Home">
+                            Home <span className="sr-only">(current)</span>
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link text-white" to="About">
+                            About
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link text-white" to="Services">
+                            Services
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link text-white" to="WorkSpace">
+                            Workspaces
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link text-white" to="Recommendation">
+                            Surf
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link  text-white" to="Contact">
+                            Contact Us
+                        </Link>
+                    </li>
+                </ul>
+                <ul className="list-unstyled d-flex mb-lg-0">
+                    <div className="social-Links d-flex align-items-center">
+                        <i className="fab fa-facebook"></i>
+                        <i className="fab fa-instagram mx-3"></i>
+                        <i className="fab fa-twitter"></i>
+                    </div>
+
+                    <li className="nav-item mx-2">
+                        <Link className="nav-link fa fa-user" to="UserProfile"></Link>
+                    </li>
+                    <li className="nav-item mx-2">
+                        <Link
+                            className="nav-link fa fa-users-gear"
+                            to="OwnerProfile"
+                        ></Link>
+                    </li>
+                    {(Cookies.expires >= Date.now()) ? <li></li>
+
+                        : <li className="nav-item mx-2">
+                            <Link className="nav-link" to="Register">
+                                Register
                             </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link text-white" to="About">
-                                About
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link text-white" to="Services">
-                                Services
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link text-white" to="WorkSpace">
-                                Workspaces
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link text-white" to="Recommendation">
-                                Surf
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link  text-white" to="Contact">
-                                Contact Us
-                            </Link>
-                        </li>
-                    </ul>
-                    <ul className="list-unstyled d-flex mb-lg-0">
-                        <div className="social-Links d-flex align-items-center">
-                            <i className="fab fa-facebook"></i>
-                            <i className="fab fa-instagram mx-3"></i>
-                            <i className="fab fa-twitter"></i>
-                        </div>
 
-                        <li className="nav-item mx-2">
-                            <Link className="nav-link fa fa-user" to="UserProfile"></Link>
-                        </li>
-                        <li className="nav-item mx-2">
-                            <Link
-                                className="nav-link fa fa-users-gear"
-                                to="OwnerProfile"
-                            ></Link>
-                        </li>
-                        {(Cookies.get("token")) ?
-                            <li></li>
+                    }
+                    {(Cookies.expires >= Date.now()) ? <li></li>
 
-                            :
-                            <li className="nav-item mx-2">
-                                <Link className="nav-link" to="Register">
-                                    Register
-                                </Link>
-                            </li>
-
-                        }
-                        {(Cookies.get("token")) ?
-                            <li></li>
-
-                            :
-                        <li className="nav-item mx-2">
+                        : <li className="nav-item mx-2">
                             <Link className="nav-link" to="Login">
                                 Login
                             </Link>
-                        </li>
-                        }
-                        <li className="nav-item mx-2">
-                            <Link className="nav-link" onClick={Logout} to="Login">
-                                Logout
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                        </li>}
+                    {(Cookies.expires <= Date.now()) ? <li></li> : <li className="nav-item mx-2">
+                        <Link className="nav-link" onClick={Logout} to="Login">
+                            Logout
+                        </Link>
+                    </li>}
+                </ul>
             </div>
-        </nav>
-    );
+        </div>
+    </nav>);
 }

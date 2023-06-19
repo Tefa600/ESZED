c8f5caa63d80cdb041f7a4c883c0ba994ce8fb9f
Aya Elsherif <96848805+ayaelsherif21@users.noreply.github.com>
2023-06-19 18:45:53 +0200

    cards

--- src/components/Recommendation/Cards/Cards.jsx
+++ src/components/Recommendation/Cards/Cards.jsx
@@ -1,48 +1,50 @@
-import React, { useEffect, useState } from "react";
-import CardsUI from "./CardsUI";
-import "./Cards.css";
-import img1 from "../../../images/eco.jpg";
-import img2 from "../../../images/creativo-3.jpg";
-import img3 from "../../../images/workspace2.jpg";
-import axios from "../../../api/axios";
-import { Row, Col } from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
+import CardsUI from './CardsUI';
+import img1 from '../../../images/eco.jpg';
+import img2 from '../../../images/creativo-3.jpg';
+import img3 from '../../../images/workspace2.jpg';
+import axios from '../../../api/axios';
+import { Row, Col } from 'react-bootstrap';
+import { Grid } from "rsuite";
 
 function Cards() {
   const [cardData, setCardData] = useState([]);
 
   useEffect(() => {
-    axios.get("api/places/getAllPlaces").then((response) => {
+    axios.get('api/places/getAllPlaces').then((response) => {
       setCardData(response.data.data.places);
       console.log(response.data.data.places);
-
       console.log(response.data.data.places[10]._id);
     });
   }, []);
-
-  console.log("number of spaces", cardData.length);
-  window.sessionStorage.setItem("spacesNumbers", cardData.length);
 
   if (!cardData) {
     return <div className="m-lg-3">Loading...</div>;
   }
 
   return (
-    <Row className="justify-content-center">
-      {cardData.map((place) => (
-        <Col key={place.id} xs={12} sm={6} md={4} lg={3}>
-          <CardsUI
-            imgsrc={place.placePhotos}
-            title={place.placeName}
-            price={place.hourPrice}
-            vip={place.vipHourPrice}
-            location={place.zone}
-            capacity={place.numberOfSeats}
-            phone={place.number}
-            id={place._id}
-          />
-        </Col>
-      ))}
-    </Row>
+   <>
+    <div  style={{ display: "flex" }}>
+        <Grid className="row row-cols-3 m-auto mt-1" style={{width:"fit-content", display: "flex",marginLeft: "10rem" }}>
+          {cardData.map((pace) => (
+            <div key={pace.id}>
+              <div style={{ display: "flex" ,width: "auto"}}>
+                <CardsUI
+                  imgsrc={pace.placePhotos}
+                  title={pace.placeName}
+                  price={pace.hourPrice}
+                  vip={pace.vipHourPrice}
+                  location={pace.zone}
+                  capacity={pace.numberOfSeats}
+                  phone={pace.number}
+                  id={pace._id}
+                />
+              </div>
+            </div>
+          ))}
+        </Grid>
+      </div>
+   </>
   );
 }
 
